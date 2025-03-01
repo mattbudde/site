08e60addca436b2ef16e11cda8b35187217b076e
mattbudde <matt@designbymatt.org>
2025-03-01 14:35:43 +0000

    Biome setup and run

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,6 @@ export default async function Home() {
           </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
